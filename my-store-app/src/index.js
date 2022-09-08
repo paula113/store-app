fc4132a75059465a8301bcb2fd3c38fc49cda3e7
paula113 <paulapierina96@gmail.com>
2022-09-08 17:17:10 -0500

    feat(index): add react query and setup

--- my-store-app/src/index.js
+++ my-store-app/src/index.js
@@ -2,11 +2,17 @@ import {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient()
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-  <App />
+    <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
 
