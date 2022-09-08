7bbd8f92c885c184e0de6d029f094cda80e97646
paula113 <paulapierina96@gmail.com>
2022-09-08 17:21:55 -0500

    feat(home): fetch products

--- my-store-app/src/containers/home.jsx
+++ my-store-app/src/containers/home.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import ResponsiveAppBar from '../Components/app-bar'
+import { getProducts } from '../queries'
+import { useQuery } from '@tanstack/react-query'
 
 const Home = () =>{
 
+  const { status, data, error } = useQuery(['products'], getProducts)
 
-    
+  if (status === 'loading') {
+    return <span>Loading...</span>
+  }
+
+  if (status === 'error') {
+    return <span>Error: {error.message}</span>
+  }
   return (
     <>
-    <ResponsiveAppBar/>
-    Home
+      <ResponsiveAppBar />
+      <ul>
+      {data.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
     </>
+  
   )
 }
 export default Home;
