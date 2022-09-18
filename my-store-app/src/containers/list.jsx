4e30b34ef508a21ee18b99eb2e504369fef3adfc
paula113 <paulapierina96@gmail.com>
2022-09-18 11:01:23 -0500

    feat(Lis): Add components

--- my-store-app/src/containers/list.jsx
+++ my-store-app/src/containers/list.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { Fragment, useState, useRef } from 'react';
 import ResponsiveAppBar from '../Components/app-bar'
+import { getProductsList } from '../queries'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Stack from '@mui/material/Stack';
+import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 const List = () =>{
 
