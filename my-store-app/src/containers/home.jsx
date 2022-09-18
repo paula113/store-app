7b120c0171c0c46295c4af8b3a5c8106119d8729
paula113 <paulapierina96@gmail.com>
2022-09-18 10:59:24 -0500

    feat(Home): Add card to display data

--- my-store-app/src/containers/home.jsx
+++ my-store-app/src/containers/home.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ResponsiveAppBar from '../Components/app-bar'
 import { getProducts } from '../queries'
 import { useQuery } from '@tanstack/react-query'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const Home = () =>{
 
@@ -17,11 +23,31 @@ const Home = () =>{
   return (
     <>
       <ResponsiveAppBar />
-      <ul>
+
+      <Grid2 container spacing={{ xs: 2, md: 3, lg: 3 }} mt={2} columns={{ xs: 4, sm: 8, md: 12, lg: 12}}>
       {data.map(product => (
-        <li key={product.id}>{product.title}</li>
+      <Grid2 xs={2} sm={4} md={4} lg={3} key={product.id} >
+      <Card  sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={product.image}
+            height="200"
+            alt={product.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="body1" component="div">
+            {product.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             {product.description.slice(0, 150)}...
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card>
+      </Grid2>
       ))}
-    </ul>
+      </Grid2>
     </>
   
   )
