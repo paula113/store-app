fe717a02edba15a4a4e48720d628d429e5003e62
paula113 <paulapierina96@gmail.com>
2022-09-18 10:56:42 -0500

    hotFix(app-bar): fix unique keys

--- my-store-app/src/Components/app-bar.jsx
+++ my-store-app/src/Components/app-bar.jsx
@@ -122,7 +122,7 @@ const ResponsiveAppBar = () => {
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
-                key={page}
+                key={page.name}
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'white', display: 'block' }}
                 component={Link}
