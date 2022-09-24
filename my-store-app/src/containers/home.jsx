ea0e81f50543744a6933e39335a67dcd6db75f48
paula113 <paulapierina96@gmail.com>
2022-09-23 21:10:00 -0500

    refactor(home): Add useProduct hook

--- my-store-app/src/containers/home.jsx
+++ my-store-app/src/containers/home.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import ResponsiveAppBar from '../Components/app-bar'
-import { getProducts } from '../queries'
-import { useQuery } from '@tanstack/react-query'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import { useProducts } from '../hooks/useProducts'
+import Container from '@mui/material/Container';
 
 const Home = () =>{
+ const {  products, isError, isLoading, isSuccess } = useProducts();
 
-  const { status, data, error } = useQuery(['products'], getProducts)
-
-  if (status === 'loading') {
-    return <span>Loading...</span>
-  }
-
-  if (status === 'error') {
-    return <span>Error: {error.message}</span>
-  }
   return (
     <>
       <ResponsiveAppBar />
-
-      <Grid2 container spacing={{ xs: 2, md: 3, lg: 3 }} mt={2} columns={{ xs: 4, sm: 8, md: 12, lg: 12}}>
-      {data.length > 0 && (data.map(product => (
+      <Container maxWidth="lg"  sx={{mt:12}}>
+      {isLoading ?? <p>Loading...</p>}
+      {isError ??  <p>Error...</p>}
+     {isSuccess && ( <Grid2 container spacing={{ xs: 2, md: 3, lg: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12}}>
+      {products.map(product => (
       <Grid2 xs={2} sm={4} md={4} lg={3} key={product.id} >
       <Card  sx={{ maxWidth: 345 }}>
         <CardActionArea>
@@ -46,8 +40,9 @@ const Home = () =>{
         </CardActionArea>
         </Card>
       </Grid2>
-      )))}
-      </Grid2>
+      ))}
+      </Grid2>)}
+      </Container>
     </>
   
   )
